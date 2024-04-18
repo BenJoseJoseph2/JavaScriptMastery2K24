229e2491f11f748de7359592beba62f6a016b1cf
BenJoseJoseph02 <benjosejoseph563@gmail.com>
2024-04-18 23:45:12 +0530

    Conversion-Js

---
+++ dataTypeConversion.js
@@ -0,0 +1,22 @@
+let score = "33";
+console.log(typeof score);
+console.log(typeof(score));
+
+// console.log(typeof score)
+
+
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+// Remember this :
+let score2 = "!myf24";
+let valueInNumber2 = Number(score2);
+console.log(valueInNumber2);
+
+let bool = true ;
+let valueInNumber3 = Number(bool);
+console.log(valueInNumber3);
+
+//  true -> 1 false -> 0  null-> 0
