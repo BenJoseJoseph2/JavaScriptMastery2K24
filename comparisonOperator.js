c3594822723f09f895c92caae9b24d159a82c4a5
BenJoseJoseph02 <benjosejoseph563@gmail.com>
2024-04-18 23:58:30 +0530

    ComparisonOperator

---
+++ comparisonOperator.js
@@ -0,0 +1,8 @@
+let res1 = 10;
+let res2 = "10";
+// Loose equality : 
+console.log(res1 == res2);
+// Strict equality : 
+console.log(res1 === res2);
+
+// Other operators are : >,<,<=,>=,!,!=,==,=,=== etc....
